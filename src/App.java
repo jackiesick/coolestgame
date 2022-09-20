1a33b7c2b8b2ccd60f851ed2b4a2c8e4c40ea779
ThickSheep <88879861+imrobertandkool@users.noreply.github.com>
2022-09-20 18:51:37 +0200

    ye

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
-public class App {
+public class App 
+{
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        int x = 12;
+
+        System.out.println(x);
     }
 }
