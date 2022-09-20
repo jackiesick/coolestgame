b15c5976f16e07ef27913255cec2cc6449c74ae1
jackiesick <113995685+jackiesick@users.noreply.github.com>
2022-09-20 19:07:28 +0200

    new

--- src/App.java
+++ src/App.java
@@ -1,15 +1,8 @@
 public class App 
 {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception
+{
+    boolean game
 
-        System.out.println("Hello, World!");
-
-        int jack = 0;
-        System.out.println(jack);
-        int x = 12;
-
-        System.out.println(x);
-
-    }
-
+        
 }
