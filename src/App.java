f71e242c1605d9498bb66ccafa0e188849d3ce29
jackiesick <113995685+jackiesick@users.noreply.github.com>
2022-09-20 18:53:33 +0200

    jack

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        int jack = 0 
+        System.out.println(jack);
     }
 }
