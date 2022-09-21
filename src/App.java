68f147c374a6eb344cb18ba3117ad8ab76e10624
ThickSheep <88879861+imrobertandkool@users.noreply.github.com>
2022-09-21 13:37:47 +0200

    added timer kaka

--- src/App.java
+++ src/App.java
@@ -1,8 +1,46 @@
-public class App 
+public class App implements Runnable
 {
-    public static void main(String[] args) throws Exception
+    public void run()
+    {
+        for(int i = 1; i <= 5; i++)
+        {
+            System.out.println(i);
+
+            try{
+                Thread.sleep(1000);
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    
+
+    public static void main(String[] args) 
 {
-    boolean game
+    Runnable r1 = new App();
+
+    Thread timer = new Thread(r1);
+
+
+
+    timer.start();
+
+    for(int i = 1; i <= 5; i++)
+        {
+            System.out.println("poes");
+
+            try{
+                Thread.sleep(1000);
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+
+        }
 
         
+}
+
+
 }
